fc706e85eb17fa612182ca306d716d7f8913ba60
qhy040404 <qhy040404@163.com>
2024-04-20 15:03:58 +0800

    use arguments to provider familyname

--- src/Snap.Hutao.Elevated.Launcher/Snap.Hutao.Elevated.Launcher.cpp
+++ src/Snap.Hutao.Elevated.Launcher/Snap.Hutao.Elevated.Launcher.cpp
@@ -1,15 +1,28 @@
+#define _CRT_SECURE_NO_WARNINGS
 #pragma comment(linker, "/SUBSYSTEM:windows /ENTRY:mainCRTStartup")
 #include <Windows.h>
+#include <string>
 
-int main()
+static std::wstring strtowstr(const char *str)
 {
+	size_t size = std::strlen(str);
+	std::wstring wstr(size, 0);
+	mbstowcs(&wstr[0], str, size);
+	return wstr;
+}
+
+int main(int argc, char* argv[])
+{
+	auto pkgFamilyName = argc == 1 ? L"60568DGPStudio.SnapHutao_wbnnev551gwxy" : strtowstr(argv[1]);
+	auto lpFile = L"shell:AppsFolder\\" + pkgFamilyName + L"!App";
+
 	SHELLEXECUTEINFO sei{};
 	sei.cbSize = sizeof(SHELLEXECUTEINFO);
 	sei.fMask = SEE_MASK_NOCLOSEPROCESS;
 	sei.hInstApp = NULL;
 	sei.hwnd = NULL;
 	sei.lpDirectory = NULL;
-	sei.lpFile = L"shell:AppsFolder\\60568DGPStudio.SnapHutao_wbnnev551gwxy!App";
+	sei.lpFile = lpFile.c_str();
 	sei.lpParameters = L"";
 	sei.lpVerb = L"runas";
 	sei.nShow = SW_SHOW;
